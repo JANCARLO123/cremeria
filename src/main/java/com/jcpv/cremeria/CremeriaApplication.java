b19e43a6ab2562faf088046babf350b0b5e53ebc
Jan Carlo Perez <carlo.vallejo@citi.com.mc>
2018-02-14 10:59:35 -0600

    Se Inicia el proyecto

---
+++ src/main/java/com/jcpv/cremeria/CremeriaApplication.java
@@ -0,0 +1,12 @@
+package com.jcpv.cremeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CremeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CremeriaApplication.class, args);
+	}
+}
